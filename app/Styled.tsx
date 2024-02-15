687d1cff0725e8ba414e5017b093f2e5113acb99
karasu_corp <aquillae777@gmail.com>
2024-02-15 20:28:54 -0300

    Configurando o ambiente

---
+++ app/Styled.tsx
@@ -0,0 +1,12 @@
+import { constrainedMemory } from "process";
+import styled from "styled-components";
+const Navbar = styled.div({})
+/**/
+const Container1 = styled.div({})
+/**/
+const Container2 = styled.div({})
+/**/
+const Container3 = styled.div({})
+/**/
+const Foooter = styled.div({})
+/**/
