2d6638bdde8449302abada7348b5f2ab2f1f3482
karasu_corp <aquillae777@gmail.com>
2024-02-18 20:37:40 -0300

    font

--- Componentes/Parte2.tsx
+++ Componentes/Parte2.tsx
@@ -39,7 +39,7 @@ export const Parte2 = (props: Props) => {
   }
   return (
     <Container2>
-              <Titulo2>Cadastro </Titulo2>
+              <Titulo2>CADASTRO </Titulo2>
       <form onSubmit={onSumitBTN}>
         <ContainerForm>
         <Label>Nome </Label>
@@ -52,7 +52,7 @@ export const Parte2 = (props: Props) => {
           <Input type="text" value={telefone} onChange={TelefoneChange}></Input>
         </ContainerForm>
         <ContainerButton>
-        <ButtonCadastro type="submit" value="add">Cadastro</ButtonCadastro>
+        <ButtonCadastro type="submit" value="add">CADASTRAR</ButtonCadastro>
         </ContainerButton>
   
       </form>
